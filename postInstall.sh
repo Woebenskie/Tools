d3347b8692088bf2d28344fe60e9476483d0faf9
Jacco van Koll <jacco.van.koll@gmail.com>
2021-10-22 14:07:12 +0200

    Update postInstall.sh
    Compress initramfs option added

--- postInstall.sh
+++ postInstall.sh
@@ -26,6 +26,7 @@ systemctl disable ksmtuned.service
 ####################################################
 # Use "eth0", "eth1" names for network devices
 ####################################################
+echo "COMPRESS=xz" > /etc/initramfs-tools/conf.d/compress
 sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub 
 update-grub 
 
